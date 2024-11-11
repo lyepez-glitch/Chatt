15b3be3977e53f0f8ad33a18db6aee0662b25060
Lucas Yepez <lucasyepez99@gmail.com>
2024-11-11 11:18:32 -0600

    Fix static files configuration

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -119,7 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# This is the directory where collected static files will be stored.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Ensure that the WhiteNoise middleware is enabled for serving static files.
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
